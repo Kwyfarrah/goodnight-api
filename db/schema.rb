a195d0d0f281785b80745ed3907aa80e009d1347
Wanying Kwok <kwyfarrah@gmail.com>
2020-08-23 14:28:42 +0900

    add the token

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_23_034626) do
+ActiveRecord::Schema.define(version: 2020_08_23_052822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 2020_08_23_034626) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.boolean "admin"
+    t.string "authentication_token", limit: 30
+    t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
